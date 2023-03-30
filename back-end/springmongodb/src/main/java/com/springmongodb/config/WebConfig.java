16178eda398153068046966e82cc9917e3f6126c
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-03-30 20:33:11 -0300

    Refactored WebConfig

---
+++ back-end/springmongodb/src/main/java/com/springmongodb/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.springmongodb.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(allowedOrigins);
+    }
+}
