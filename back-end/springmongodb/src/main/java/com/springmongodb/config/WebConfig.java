b8c3be5245b8358f7283c1f8d8ae28ac43e011c2
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-05-04 11:51:45 -0300

    Refactored configuration

--- back-end/springmongodb/src/main/java/com/springmongodb/config/WebConfig.java
+++ back-end/springmongodb/src/main/java/com/springmongodb/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowedOrigins}")
+    @Value("${cors.allowed-origins}")
     private String allowedOrigins;
 
     @Override
