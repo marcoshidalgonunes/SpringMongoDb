1c1566cc75bb2d142b60f0867ce30674a1a5b1db
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-05-19 18:35:11 -0300

    Fixed oauth2ResourceServer configuration

--- back-end/springmongodb/src/main/java/com/springmongodb/config/SecurityConfig.java
+++ back-end/springmongodb/src/main/java/com/springmongodb/config/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.springmongodb.config;
 
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuerUri;
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -27,11 +23,8 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()     
             )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
-                .decoder(JwtDecoders.fromIssuerLocation(issuerUri))
-            )
-        );
-
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+             
         return http.build();
     }
 
